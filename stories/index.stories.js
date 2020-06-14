2adae7f4307511c42008043eb0e5c3646c3b5d45
Lucas Oliveira <iamtheolivetree@gmail.com>
2020-06-14 13:50:49 -0300

    feature: Hello World Storybook

---
+++ stories/index.stories.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Components',
+}
+
+export const Button = () => '<button> button </button>'
+
+export const Input = () => '<input type="text">'
+
+export const Checkbox = () => '<input type="checkbox"> Item'
+
+export const Image = () => '<img width="200" alt="pikachu" src="https://pikachublx.files.wordpress.com/2015/05/cropped-pikachu.png">'
+
+export const Progressbar = () => '<progress max="100" value="70"></progress>'
